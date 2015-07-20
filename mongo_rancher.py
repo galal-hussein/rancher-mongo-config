6f0bfe1f555b4a9b0957750878b91b2ab01b837a
HusseinGalal <hussein.galal.ahmed.11@gmail.com>
2015-07-20 23:09:22 +0200

    finalizing the script

--- mongo_rancher.py
+++ mongo_rancher.py
@@ -1,6 +1,14 @@
+import subprocess
 import pymongo
 from pymongo import MongoClient
-
+import time
+#service name is the name of the service link in rancher
+#it is used as an argument to fetch the containers from Rancher's API
+#and to add it to the replica set
+#
+#for ex: service name: mongo
+#service id will be the id of the mongo service in Rancher
+#and the desired server to add to the replica set is mongo server
 def mongo_check(rancher_server, service_name):
     client = MongoClient('mongodb://'+service_name)
     db = client.db
@@ -23,13 +31,22 @@ def mongo_connect(ranchersrv,service_name):
     url = 'http://'+ranchersrv+'/v1/projects/1a5/services/'+service_id+'/instances'
     service_resp = requests.get(url=url)
     service_data = json.loads(service_resp.text)['data']
+    # also it is assumed that we use 27017 port for now
     mongo_address = service_data[0]['primaryIpAddress']+":27017"
     client = MongoClient('mongodb://'+mongo_address)
     db = client.db
     ismaster= db.command('isMaster')
-    client_primary = MongoClient('mongodb://'+ismaster['primary'])
+    mongo_primary = ismaster['primary'].split(':')[0]
+    port=ismaster['primary'].split(':')[1]
+    task="rs.add('"+service_name+"')"
+    print subprocess.call(["/usr/bin/mongo", "--host", str(mongo_primary),"--port", str(port), "--eval", task])
 
+def checking_mongo(rancher_server, service_name):
+    while True:
+        time.sleep(10)
+        mongo_check(rancher_server, service_name)
 
-
-
-
+if __name__ == "__main__":
+    rancher_server = os.environ['RANCHER_SERVER']
+    service_name = os.environ['MONGO_SERVICE_NAME']
+    checking_mongo(rancher_server, service_name)
