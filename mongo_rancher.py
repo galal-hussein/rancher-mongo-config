ee22ab0633d2152f29e89c67d37e546cf1051680
HusseinGalal <hussein.galal.ahmed.11@gmail.com>
2015-07-21 22:43:41 +0200

    add host

--- mongo_rancher.py
+++ mongo_rancher.py
@@ -2,6 +2,7 @@ import subprocess
 import pymongo
 from pymongo import MongoClient
 import time
+import netifaces
 #service name is the name of the service link in rancher
 #it is used as an argument to fetch the containers from Rancher's API
 #and to add it to the replica set
@@ -32,19 +33,39 @@ def mongo_connect(ranchersrv,service_name):
     service_resp = requests.get(url=url)
     service_data = json.loads(service_resp.text)['data']
     # also it is assumed that we use 27017 port for now
-    mongo_address = service_data[0]['primaryIpAddress']+":27017"
-    client = MongoClient('mongodb://'+mongo_address)
+    #choose random mongo server
+    random_mongo_address = service_data[0]['primaryIpAddress']+":27017"
+    client = MongoClient('mongodb://'+random_mongo_address)
     db = client.db
     ismaster= db.command('isMaster')
+    # primary mongo server
     mongo_primary = ismaster['primary'].split(':')[0]
     port=ismaster['primary'].split(':')[1]
-    task="rs.add('"+service_name+"')"
+    # targeting the mongo server in the same host
+    mongo_host = get_host(ranchersrv)
+    for i in  range(len(service_data)):
+        if service_data[i]['requestedHostId'] == mongo_host:
+            mongo_samehost = service_data[i]['primaryIpAddress']
+            break
+    task="rs.add('"+mongo_samehost+"')"
     print subprocess.call(["/usr/bin/mongo", "--host", str(mongo_primary),"--port", str(port), "--eval", task])
 
 def checking_mongo(rancher_server, service_name):
     while True:
         time.sleep(10)
         mongo_check(rancher_server, service_name)
+
+def get_host(ranchersrv):
+    #get host for the running container
+    ipaddress = netifaces.ifaddresses('eth0')[netifaces.AF_INET][1]['addr']
+    project_url = 'http://'+ranchersrv+'/v1/projects/1a5/containers'
+    project_resp = requests.get(url=project_url)
+    project_data = json.loads(project_resp.text)['data']
+    for i in range(len(project_data)):
+        if project_data[i]['primaryIpAddress'] == str(ipaddress):
+            hostid = project_data[i]['requestedHostId']
+            break
+    return hostid
 
 if __name__ == "__main__":
     rancher_server = os.environ['RANCHER_SERVER']
