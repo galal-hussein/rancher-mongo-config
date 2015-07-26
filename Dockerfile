3535c1a731c7695d14a41e10f4ba0db29f37dd18
Hussein Galal <hussein.galal.ahmed.11@gmail.com>
2015-07-26 19:22:26 -0400

    version 2

--- Dockerfile
+++ Dockerfile
@@ -1,18 +1,18 @@
-FROM ubuntu:14.04
+FROM mongo:latest
 MAINTAINER Hussein Galal
 
-RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 \
-&& echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list \
-&& apt-get update \
-&& apt-get install -y mongodb-org-shell=3.0.4 \
+RUN apt-get update \
 && apt-get install -yqq python build-essential python-dev python-pip python-setuptools \
 && pip install pymongo \
-&& pip install netifaces
+&& pip install netifaces \
+&& pip install dnspython
 
 RUN mkdir -p /mongo
 WORKDIR /mongo
 
 ENV MONGO_SERVICE_NAME mongo
-ADD mongo_rancher.py mongo_rancher.py
+ADD mongo_run.sh mongo_run.sh
+RUN chmod u+x mongo_run.sh
 
-CMD ["python", "mongo_rancher.py"]
+ENTRYPOINT /bin/bash mongo_run.sh
+CMD []
---
+++ mongo_run.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "Running the Connecting script"
+cat << EOF > mongo_replica.py
+#!/usr/bin/python
+import subprocess
+import pymongo
+import os
+import socket
+from pymongo import MongoClient
+import time
+import netifaces
+import dns.resolver
+import sys
+
+def mongo_connect(service_name,myip):
+    arecords = dns.resolver.query(service_name,'A')
+    if len(arecords) <= 3:
+	print "The number of Mongo servers is less than 3..can't connect"
+	sys.exit(0)
+    # also it is assumed that we use 27017 port for now
+    #choose random mongo server
+    random_mongo_address = str(arecords[0])+":27017"
+    client = MongoClient('mongodb://'+random_mongo_address)
+    db = client.db
+    ismaster= db.command('isMaster')
+    # primary mongo server
+    mongo_primary = ismaster['primary'].split(':')[0]
+    port=ismaster['primary'].split(':')[1]
+    task="rs.add('"+myip+"')"
+    try:
+	subprocess.call(["/usr/bin/mongo", "--host", str(mongo_primary),"--port", str(port), "--eval", task])
+    except ValueError as err:
+	print(err.msg)
+
+def get_myip():
+    #get ip of the running container
+    ipaddress = netifaces.ifaddresses('eth0')[netifaces.AF_INET][1]['addr']
+    return ipaddress
+
+if __name__ == "__main__":
+    service_name = os.environ['MONGO_SERVICE_NAME']
+    myip = get_myip()
+    mongo_connect(service_name,myip)
+    
+EOF
+chmod u+x mongo_replica.py
+./mongo_replica.py
+
+if [ $? -eq 0 ]
+then
+/entrypoint.sh
+mongod
+else
+echo "Error Occurred.."
+fi
