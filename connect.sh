adf91193fd6e7e33e3b809d17d513330ca65d8f0
galal-hussein <hussein.galal.ahmed.11@gmail.com>
2015-11-19 22:25:33 -0500

    Fix waiting problem

--- connect.sh
+++ connect.sh
@@ -9,6 +9,7 @@ function cluster_init {
 	for member in $(cat ips.tmp); do
 		if [ $member != $MYIP ]; then
 			mongo --eval "printjson(rs.add('$member:27017'))"
+			sleep 5
 		fi
 	done
 
@@ -21,8 +22,8 @@ function find_master {
 		if echo $IS_MASTER | grep "true"; then
 			return 0
 		fi
-	return 1
 	done
+	return 1
 }
 # Script starts here
 # wait for mongo to start
@@ -30,6 +31,9 @@ while [ `$DIG A $MONGO_SERVICE_NAME +short | wc -l` -lt 3 ]; do
 	echo 'mongo instances are less than 3.. waiting!'
 	sleep 5
 done
+
+# Wait until all services are up
+sleep 10
 find_master
 if [ $? -eq 0 ]; then
 	echo 'Master is already initated.. nothing to do!'
